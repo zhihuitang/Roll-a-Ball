1c7a02b2e73f5534c60e2754ff76a5c70006d366
Zhihui Tang <crafttang@gmail.com>
2017-03-12 21:38:24 +0100

    collider

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,4 +18,12 @@ public class PlayerController : MonoBehaviour {
 		rb.AddForce (movement * speed);
 
 	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+		}
+	}
 }
