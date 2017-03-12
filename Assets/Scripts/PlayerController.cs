a43024fe4d4a31ac3b046c38e35f51077cda2bb0
Zhihui Tang <crafttang@gmail.com>
2017-03-12 22:09:00 +0100

    finish the game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 	private Rigidbody rb;
 	public float speed;
+	private int count;
+	public Text countText;
+	public Text winText;
 
 	void Start(){
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		winText.text = "";
+		SetCountText ();
+
 	}
 
 	void FixedUpdate(){
@@ -24,6 +32,15 @@ public class PlayerController : MonoBehaviour {
 		if (other.gameObject.CompareTag ("Pick Up"))
 		{
 			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You Win!";
 		}
 	}
 }
